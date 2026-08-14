99feb6632e73cf4218d28d9678dab471224b8bd0
Oleg122 <joeburen122@gmail.com>
2026-08-14 08:56:36 +0300

    repare csrf

--- src/services/checkAndSetCookie.ts
+++ src/services/checkAndSetCookie.ts
@@ -1,10 +1,20 @@
-import { getCookieName } from "./getCookieName";
 import { setCookie } from "./setCookie";
 
-export function checkAndSetCSRFToken() {
-    const csrfToken = getCookieName('csrftoken');
-    if (!csrfToken) {
-        setCookie('csrftoken', '', 1);
-    } else {
-    } 
+export async function checkAndSetCSRFToken() {
+    const response = await fetch(`${__BACKEND_URL__}/csrf/`, {
+        method: 'GET',
+        credentials: 'include'
+    });
+
+    if (!response.ok) {
+        throw new Error(
+            `Failed to get CSRF token: ${response.status}`
+        );
+    }
+
+    const data = await response.json();
+
+    setCookie('csrftoken', data.csrfToken, 1);
+
+    return data.csrfToken;
 }
